4301f20681c66ad09236346e45ff86d45d830bdc
jelson <jelson20@outlook.com>
2022-06-22 09:41:12 +0100

    add classes

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print("hello world!")
+emp1 = ["Tester", "Joe Doe" "age", 8200]
+emp2 = ["Front-end", "Smith Jeffery" "age", 8200]
+
+
+class Employee:
+
+    def __init__(self, name, email, age, payment, jobTitle):
+
+        # instance attributes
+
+        self.name = name
+        self.email = email
+        self.age = age
+        self.jobTitle = jobTitle
+        self.payment = payment
+
+
+# instance
+emp1 = Employee("George", "georgr@example.com", 41, "Backend", 7855)
+print(emp1.name, emp1.email)
