67e87bde70cf05be79c9e4cd873561f3c645ee20
jelson <jelson20@outlook.com>
2022-06-22 11:10:50 +0100

    update app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-emp1 = ["Tester", "Joe Doe" "age", 8200]
-emp2 = ["Front-end", "Smith Jeffery" "age", 8200]
+emp1 = ["Tester", "geo@example.com", "Joe Doe", 25, 8200, "jobTitle"]
+emp2 = ["Front-end", "jeffery@example.com", "Jeffery", 25, 98200, "jobTitle"]
+emp3 = ["Web Developer", "jay@example.com", "Jay", 35, 3200, "jobTitle"]
 
 
 # class
@@ -7,15 +8,17 @@ class Employee:
 
     def __init__(self, name, email, age, payment, jobTitle):
 
-        # instance attributes
-
+     # instance attributes
         self.name = name
         self.email = email
         self.age = age
-        self.jobTitle = jobTitle
         self.payment = payment
+        self.jobTitle = jobTitle
 
 
 # instance
-emp1 = Employee("George", "georgr@example.com", 41, "Backend", 7855)
-print(emp1.name, emp1.email)
+emp1 = Employee("George", "george25@example.com", 25, "Backend", 7855)
+print(emp1.name, emp1.email, emp1.age, emp1.jobTitle, emp1.payment)
+emp2 = Employee("Jeffery", "jeffery@example.com", 25, "Front-end", 8200)
+print(emp2.name, emp2.email, emp2.jobTitle,
+      emp2.payment, emp2.jobTitle, emp2.payment)
