5a98906d7fc65dbb324f761bb829d73c8682b7f6
jelson <jelson20@outlook.com>
2022-06-23 11:02:59 +0100

    commit changes

--- app3.py
+++ app3.py
@@ -8,20 +8,31 @@ class Employee:
     def work(self):
         print(f"{self.name} is a hard worker, derserve a promotion!")
 
+# extends the function employee
+
 
 class JobType(Employee):
-    pass
+    def __init__(self, name, email, salary, tester, designer, backend, frontend):
+        super().__init__(name, email, salary)
+        self.tester = tester
+        self.designer = designer
+        self.backend = backend
+        self.frontend = frontend
 
 
 class Developer(Employee):
     pass
 
 
-job = JobType("Joe Deo", "jeo4@gmail.com", 2500)
+job = JobType("Joe Deo", "jeo4@gmail.com", 2500,
+              "Tester", "Designer", "Back-End", "Front-End")
 developer = Developer("Jazz", "jazz@example.com", 3500)
 
 print(job.name, job.email, job.salary)
 job.work()
-print("From developer class")
+print(job.tester, job.designer, job.backend, job.frontend)
+
+
+print("From developer class...")
 print(developer.name, developer.email, developer.salary)
 developer.work()
