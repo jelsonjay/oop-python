3d4ea4ba0ce62af64c7a8137de89200de78213c1
jelson <jelson20@outlook.com>
2022-06-24 13:38:56 +0100

    multiline & formatting strings

--- string.py
+++ string.py
@@ -1,17 +1,23 @@
-comment = """
-Lorem Ipsum is simply dummy text of the printing and 
-typesetting industry. Lorem Ipsum has been the industry's 
-standard dummy text ever since the 1500s, when an unknown 
-printer took a galley of type and scrambled it to make a 
-type specimen book.
-"""
-
-
-
-# name = "jelson j"
-# job_desc = "Front-end Developer"
-# comment = f"""
-# Hi my name is {name}, I'm a {job_desc}
+# comment = """
+# Lorem Ipsum is simply dummy text of the printing and 
+# typesetting industry. Lorem Ipsum has been the industry's 
+# standard dummy text ever since the 1500s, when an unknown 
+# printer took a galley of type and scrambled it to make a 
+# type specimen book.
 # """
 
+# name = "Jeo Deo"
+# comment = """
+# hello my name is {}
+# """
+# print(comment.format(name))
+
+
+name = "jelson j"
+age = 105
+job_desc = "Front-end Developer"
+comment = f"""
+Hi my name is {name.capitalize()}, I'm a {job_desc},
+I love my work and I'm {age} years young
+"""
 print(comment)
