0318c1e4a257c9be22d13ebce827cd2f109ec62c
jelson <jelson20@outlook.com>
2022-06-25 13:08:13 +0100

    list method

---
+++ list_methods.py
@@ -0,0 +1,14 @@
+fruts = ["apple", "mango", "per","banana"]
+fruts.sort()
+fruts.append("avacado")
+print(fruts)
+
+numbers = [22,5,36,1280,14,-9]
+numbers.sort()
+numbers.append(74)
+numbers.remove(1280)
+#numbers.clear()
+print(numbers)
+mix= ["products", 41, ["laptop", "pc", 55], 45, "mouse"]
+
+print(mix)
